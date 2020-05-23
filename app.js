6c42a5734b22fb67eec3afd8b96dfa2c4f7e1ebc
Yuchan <mikuplayer123@gmail.com>
2020-05-23 18:24:48 +0900

    Post stores data

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ import express from "express";
 const app = express();
 const PORT = 4000;
 
+app.use(express.json({
+  limit: "50mb"
+}));
+
+app.post("/api/stores", (req, res) => {
+  const dbStores = req.body;
+  console.log(dbStores)
+  res.send(dbStores)
+})
+
 app.get("/", (req, res) => {
   res.send("Oh fuck~");
 });
