6915244431f9fde1b20887284b860e3d9ebdfeed
Yuchan <mikuplayer123@gmail.com>
2020-05-24 09:05:10 +0900

    Install mongoose and connect to MongoDB

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
 import express from "express";
-
+import mongoose from "mongoose";
 const app = express();
 const PORT = 4000;
+const adminPw = "5WaAV3ePLG0TXAyi";
 
-app.use(express.json({
-  limit: "50mb"
-}));
+mongoose.connect(
+  `mongodb+srv://yuchan:${adminPw}@cluster0-nyivz.mongodb.net/test?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+app.use(
+  express.json({
+    limit: "50mb",
+  })
+);
 
 app.post("/api/stores", (req, res) => {
   const dbStores = req.body;
-  console.log(dbStores)
-  res.send(dbStores)
-})
+  console.log(dbStores);
+  res.send(dbStores);
+});
 
 app.get("/", (req, res) => {
   res.send("Oh fuck~");
 });
-
 app.listen(4000 || PORT, () =>
   console.log(`Listening on: http://localhost:${PORT}`)
 );
